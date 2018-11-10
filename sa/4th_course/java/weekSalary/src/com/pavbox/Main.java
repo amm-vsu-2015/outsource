7b60f1735bc8610c7480f92f56dfcc5270690d03
pavbox <pavboxdev@gmail.com>
2018-11-11 01:10:22 +0300

    added 1st task for sa

---
+++ sa/4th_course/java/weekSalary/src/com/pavbox/Main.java
@@ -0,0 +1,31 @@
+package com.pavbox;
+
+
+/**
+ * Написать метод, высчитывающий зарплату работника по отработанным часам и ставке.
+ * Если значение
+ */
+public class Main {
+
+    final static int MAX_HOURS_FOR_WEEK = 60;
+
+    public static void main(String[] args) {
+
+        Main.showSalaryBy(60, 100);
+        Main.showSalaryBy(45, 100);
+        Main.showSalaryBy(61, 100);
+
+    }
+
+    private static void showSalaryBy(float hours, float rate) {
+
+        if (hours <= MAX_HOURS_FOR_WEEK) {
+            float amount = hours * rate;
+            System.out.println(amount);
+        } else {
+            System.out.printf("[ # ] Can't transfer salary for %.2f hours per week.\n[ ! ] Please, enter another hours value (less than %d hours).", hours, MAX_HOURS_FOR_WEEK);
+        }
+
+    }
+
+}
