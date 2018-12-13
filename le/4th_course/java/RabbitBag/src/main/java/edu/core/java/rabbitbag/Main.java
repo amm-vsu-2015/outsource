f3702f1bc0d93395b93851d2e4c50f53f004fcbf
pavbox <pavboxdev@gmail.com>
2018-12-14 01:45:24 +0300

    added java-jar builds

--- le/4th_course/java/RabbitBag/src/main/java/edu/core/java/rabbitbag/Main.java
+++ le/4th_course/java/RabbitBag/src/main/java/edu/core/java/rabbitbag/Main.java
@@ -2,7 +2,13 @@ package edu.core.java.rabbitbag;
 
 public class Main {
 
+    // javac -d out src/main/java/edu/core/java/rabbitbag/Main.java
+    // out
+    // jar cmvf ../src/main/resources/META-INF/manifest.mf ../out/rb.jar edu/core/java/rabbitbag/Main.class
+    // cd ..
+    // java -jar ./out/rb.jar
     public static void main(String[] args) {
-	// write your code here
+        System.out.printf("hello");
     }
+
 }
