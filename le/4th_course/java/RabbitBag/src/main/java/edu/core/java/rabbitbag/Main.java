70d8fc4d1f2f6ffd30b4a1f7bd1ca0370ea11b63
pavbox <pavboxdev@gmail.com>
2018-12-23 02:25:44 +0300

    feeds flow type

--- le/4th_course/java/RabbitBag/src/main/java/edu/core/java/rabbitbag/Main.java
+++ le/4th_course/java/RabbitBag/src/main/java/edu/core/java/rabbitbag/Main.java
@@ -31,73 +31,103 @@ public class Main {
 
         char answer = 'y';
 
+        try {
+
+            while (answer != 'n') {
+
+                // todo: get actual data
+                // todo: print list of created entities on the screen (or add new)
+                // todo: ask user what type of ent will change
+                // todo: show all ent of selected type
+                // todo: show available actions
+                // todo: make some action
+                // todo: update repos & update file
+                // todo: return to 1st step
+
+                // setup loaders
+                FeedLoader feedLoader = new FeedLoader();
+                KitsLoader kitsLoader = new KitsLoader();
+
+                // get repos
+                FeedRepository feedRepository = feedLoader.getRepository();
+                KitsRepository kitsRepository = kitsLoader.getRepository();
+
+                FeedTranslator feedsTranslator = new FeedTranslator();
+                KitsTranslator kitsTranslator = new KitsTranslator();
+
+                // define conditions
+
+                List<FeedValueObject> feeds = feedRepository.findAll();
+                List<KitsValueObject> kits = kitsRepository.findAll();
+
+                System.out.println("\nWhat are we have to store in database now:");
+                System.out.println("[ ] 1. Feeds (" + feeds.size() + " in store)");
+                System.out.println("[ ] 2. Kits  (" + kits.size() + " in store)");
+
+                Character flowType = askUser("\nPlease, write number of entity for showing some actions: ");
+
+                switch (flowType) {
+                    case '1':
+                        feedsFlow(feedLoader, feedRepository, feedsTranslator);
+                        break;
+                    case '2':
+                        // todo kits flow
+                        break;
+                    default:
+                        System.out.println("\nUnfortunately, entity with this id not found...");
+                }
+
+                System.out.println("Please, try again...\n\n");
+
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    // todo maybe abstract with
+    private static void feedsFlow(FeedLoader loader, FeedRepository repository, FeedTranslator translator) throws IOException {
+
+        Character answer = 'y';
+
         while (answer != 'n') {
 
-            // todo: get actual data
-            // todo: print list of created entities on the screen (or add new)
-            // todo: ask user what type of ent will change
-            // todo: show all ent of selected type
-            // todo: show available actions
-            // todo: make some action
-            // todo: update repos & update file
-            // todo: return to 1st step
+            System.out.println("\n\n[#] 1. Feeds selected (\" + feeds.size() + \" in store).\n");
 
-            // setup loaders
-            FeedLoader feedLoader = new FeedLoader();
-            KitsLoader kitsLoader = new KitsLoader();
+            System.out.println("You have a few actions: ");
+            System.out.println("1. Add new entity.");
+            System.out.println("2. Update entity by id.");
+            System.out.println("3. Remove entity by id.");
+            System.out.println("4. Show entity list.\n");
 
-            // get repos
-            FeedRepository feedRepository = feedLoader.getRepository();
-            KitsRepository kitsRepository = kitsLoader.getRepository();
-
-            FeedTranslator feedstranslator = new FeedTranslator();
-            KitsTranslator kitsTranslator = new KitsTranslator();
-
-            // define conditions
-
-            List<FeedValueObject> feeds = feedRepository.findAll();
-            List<KitsValueObject> kits = kitsRepository.findAll();
-
-            System.out.println("What are we have to store in database now:");
-
-            if (!feeds.isEmpty()) {
-                System.out.println("Feeds: " + feeds.size());
-            }
-
-            if (!kits.isEmpty()) {
-                System.out.println("Kits: " + kits.size());
-            }
-
-            // show
-
-            feeds.forEach(feedVO -> System.out.println(feedstranslator.translate(feedVO).toJSON()));
-            System.out.println();
-            kits.forEach(kitVO -> System.out.println(kitsTranslator.translate(kitVO).toJSON()));
+            answer = askUser("Please, select your action number (or write 'n' to back): ");
             System.out.println();
 
-            // remove
-
-            feedRepository.remove(1);
-            feedRepository.findAll().forEach(feedVO -> System.out.println(feedstranslator.translate(feedVO).toJSON()));
-
-            // upload
-
-            feedLoader.upload(feedRepository);
-
-            if (kits.size() == 1) { break; }
-
+            switch (answer) {
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                    repository.findAll().forEach(feedVO -> System.out.println(translator.translate(feedVO).toJSON()));
+            }
         }
     }
 
-    private static Character askUser() throws IOException {
 
-        System.out.println("\nPlease, write char in A-Z ranges:");
+
+    private static Character askUser(String text) throws IOException {
+        System.out.print(text);
 
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        String answer = buffer.readLine().toUpperCase();
+        String answer = buffer.readLine().toLowerCase();
 
         if (answer.length() > 1) {
-            System.out.println("You wrote answer that has more length than requied. We will use only first char of string...");
+            System.out.println("You wrote large answer, we take only first char...");
         }
 
         return answer.trim().charAt(0);
