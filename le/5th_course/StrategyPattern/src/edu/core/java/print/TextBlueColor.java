f55b032db7d70ad0c73f28d644212ae8458d14d2
pavbox <pavboxdev@gmail.com>
2019-11-14 17:57:46 +0300

    strategy pattern for le

---
+++ le/5th_course/StrategyPattern/src/edu/core/java/print/TextBlueColor.java
@@ -0,0 +1,9 @@
+package edu.core.java.print;
+
+public class TextBlueColor implements IColor {
+
+    @Override
+    public void paint() {
+        System.out.println("print blue color text");
+    }
+}
---
+++ le/5th_course/StrategyPattern/src/edu/core/java/print/TextRedColor.java
@@ -0,0 +1,10 @@
+package edu.core.java.print;
+
+public class TextRedColor implements IColor {
+
+    @Override
+    public void paint() {
+        System.out.println("print red color text");
+    }
+
+}
